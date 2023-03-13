ed83c15db631c9d8e889e39a15c35b0c65553466
danieljuma5 <jumadaniel275@gmail.com>
2023-03-13 16:41:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+// Declaring companyName varaible
+const companyName = 'Scuber';
+
+
+//Declaring mostProfitableNeighborhood using let
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+//Declaring companyCeo using let
+let companyCeo = 'Susan Smith'
+
+
